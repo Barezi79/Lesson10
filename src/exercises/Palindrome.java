e815ff7efcb69b61aa725249fe25bd3bbe066339
Barezi79 <branislavnesic79@gmail.com>
2019-07-14 19:21:29 +0200

    Palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,27 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import examples.FileHelper;
 
 public class Palindrome {
 
+	List<String> words = new ArrayList<String>();
+
+	Palindrome() {
+		words = loadWords();
+	}
+
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	}
+
+	public boolean wordFind(String string) {
+		return words.contains(string);
+	}
+
+	public boolean wordIsAPalindrome(String string) {
+		return wordFind(string) && wordFind(new StringBuilder(string).reverse().toString());
 	}
 }
