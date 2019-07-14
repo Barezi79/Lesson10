068c16e27a4c837dae43b70e306f2cdc3eb7928b
Barezi79 <branislavnesic79@gmail.com>
2019-07-14 19:22:11 +0200

    PalindromeTest

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
+
 	Palindrome testPalindrome = new Palindrome();
 
 	@Test
 	void testLoadWords() {
 		assertTrue(testPalindrome.loadWords().contains("apple"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	@Test
+	void wordFind() throws Exception {
+		assertTrue(testPalindrome.loadWords().contains("oversophisticated"));
+	}
+
 	// 3. Test that a word is a palindrome
+	@Test
+	void testIsAPalindrome() throws Exception {
+		assertTrue(testPalindrome.wordIsAPalindrome("radar"));
+		assertFalse(testPalindrome.wordIsAPalindrome("Nevada"));
+		assertTrue(testPalindrome.wordIsAPalindrome("madam"));
+	}
 
 }
